b93b683149124c4159ee025e9b1d68a863463349
alexandru.danciu@rbc.com.ro <alexandru.danciu@rbc.com.ro>
2025-04-25 14:49:27 +0300

    Now starts MultiWebView

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        // Application.Run(new MultiWebViewForm());
-        Application.Run(new DualWebViewForm());
+        Application.Run(new MultiWebViewForm());
+        // Application.Run(new DualWebViewForm());
     }    
 }
